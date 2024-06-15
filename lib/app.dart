f9f0173a6587ed5762e635b322e8a0253cff92f7
realth000 <a598919335@gmail.com>
2024-06-15 23:15:48 +0800

    feat(theme): 添加默认主题

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kdays_client/routes/routes.dart';
+import 'package:kdays_client/theme/theme.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 /// 顶层应用
 class App extends StatelessWidget {
@@ -8,20 +10,20 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    final app = MaterialApp.router(
       title: 'KDays客户端',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.makeLight(context),
+      darkTheme: AppTheme.makeDark(context),
       routerConfig: router,
+    );
+
+    return ResponsiveBreakpoints.builder(
+      breakpoints: [
+        const Breakpoint(start: 0, end: 450, name: MOBILE),
+        const Breakpoint(start: 451, end: 800, name: TABLET),
+        const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+      ],
+      child: app,
     );
   }
 }
---
+++ lib/theme/theme.dart
@@ -0,0 +1,93 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+/// 全局主题
+final class AppTheme {
+  AppTheme._();
+
+  /// 做一个浅色主题，[seedColor]是可选的主题颜色
+  ///
+  /// [seedColor]为空时使用flex_color_scheme的颜色，为空时用material默认生成一套颜色。
+  static ThemeData makeLight(BuildContext context, [Color? seedColor]) {
+    ColorScheme? seedScheme;
+    if (seedColor != null) {
+      seedScheme = ColorScheme.fromSeed(seedColor: seedColor);
+    }
+    return FlexThemeData.light(
+      primary: seedScheme?.primary,
+      onPrimary: seedScheme?.onPrimary,
+      primaryContainer: seedScheme?.primaryContainer,
+      onPrimaryContainer: seedScheme?.onPrimaryContainer,
+      secondary: seedScheme?.secondary,
+      onSecondary: seedScheme?.onSecondary,
+      secondaryContainer: seedScheme?.secondaryContainer,
+      onSecondaryContainer: seedScheme?.onSecondaryContainer,
+      tertiary: seedScheme?.tertiary,
+      onTertiary: seedScheme?.onTertiary,
+      tertiaryContainer: seedScheme?.tertiaryContainer,
+      onTertiaryContainer: seedScheme?.onTertiaryContainer,
+      error: seedScheme?.error,
+      onError: seedScheme?.onError,
+      surface: seedScheme?.surface,
+      onSurface: seedScheme?.onSurface,
+      background: seedScheme?.surface,
+      onBackground: seedScheme?.onSurface,
+      surfaceTint: seedScheme?.surfaceTint,
+      scheme: seedScheme == null ? FlexScheme.barossa : null,
+      surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+      blendLevel: 7,
+      subThemesData: const FlexSubThemesData(
+        blendOnLevel: 10,
+        blendOnColors: false,
+        useM2StyleDividerInM3: true,
+        navigationRailLabelType: NavigationRailLabelType.all,
+      ),
+      visualDensity: FlexColorScheme.comfortablePlatformDensity,
+      useMaterial3: true,
+      swapLegacyOnMaterial3: true,
+    );
+  }
+
+  /// 做一个深色主题，[seedColor]是可选的主题颜色
+  ///
+  /// [seedColor]为空时使用flex_color_scheme的颜色，为空时用material默认生成一套颜色。
+  static ThemeData makeDark(BuildContext context, [Color? seedColor]) {
+    ColorScheme? seedScheme;
+    if (seedColor != null) {
+      seedScheme = ColorScheme.fromSeed(seedColor: seedColor);
+    }
+
+    return FlexThemeData.dark(
+      primary: seedScheme?.primary,
+      onPrimary: seedScheme?.onPrimary,
+      primaryContainer: seedScheme?.primaryContainer,
+      onPrimaryContainer: seedScheme?.onPrimaryContainer,
+      secondary: seedScheme?.secondary,
+      onSecondary: seedScheme?.onSecondary,
+      secondaryContainer: seedScheme?.secondaryContainer,
+      onSecondaryContainer: seedScheme?.onSecondaryContainer,
+      tertiary: seedScheme?.tertiary,
+      onTertiary: seedScheme?.onTertiary,
+      tertiaryContainer: seedScheme?.tertiaryContainer,
+      onTertiaryContainer: seedScheme?.onTertiaryContainer,
+      error: seedScheme?.error,
+      onError: seedScheme?.onError,
+      surface: seedScheme?.surface,
+      onSurface: seedScheme?.onSurface,
+      background: seedScheme?.surface,
+      onBackground: seedScheme?.onSurface,
+      surfaceTint: seedScheme?.surfaceTint,
+      scheme: seedScheme == null ? FlexScheme.barossa : null,
+      surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+      blendLevel: 13,
+      subThemesData: const FlexSubThemesData(
+        blendOnLevel: 20,
+        useM2StyleDividerInM3: true,
+        navigationRailLabelType: NavigationRailLabelType.all,
+      ),
+      visualDensity: FlexColorScheme.comfortablePlatformDensity,
+      useMaterial3: true,
+      swapLegacyOnMaterial3: true,
+    );
+  }
+}
